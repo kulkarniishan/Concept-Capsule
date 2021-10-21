f3fa494ee56546091cf8ac8f94964133bec3ba61
Ishan <ishan.kulkarni@somaiya.edu>
2021-10-21 18:47:16 +0530

    Routes added

---
+++ mooc_app/lib/router.dart
@@ -0,0 +1,17 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:mooc_app/pages/home_page.dart';
+import 'package:mooc_app/pages/login_page.dart';
+import 'package:mooc_app/routing_constants.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case HOME_ROUTE:
+      return MaterialPageRoute(builder: (context) => HomePage());
+    case LOGIN_ROUTE:
+      return MaterialPageRoute(builder: (context) => LoginPage());
+    default:
+      return MaterialPageRoute(builder: (context) => LoginPage());
+  }
+}
---
+++ mooc_app/lib/routing_constants.dart
@@ -0,0 +1,2 @@
+const String HOME_ROUTE = '/';
+const String LOGIN_ROUTE = '/login';
