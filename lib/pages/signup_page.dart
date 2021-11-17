5e852f856b80a97130bcbc8db7c1bbdfb0d3e5fa
Ishan <ishan.kulkarni@somaiya.edu>
2021-11-17 23:17:08 +0530

    test1

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -84,7 +84,7 @@ class _SignUpPageState extends State<SignUpPage> {
 
           print(userCredential);
         } on FirebaseAuthException catch (e) {
-          if (e.code == 'user-not-foun') {
+          if (e.code == 'user-not-found') {
             print('No user found for that email.');
           } else if (e.code == 'wrong-password') {
             print('Wrong password provided for that user.');
