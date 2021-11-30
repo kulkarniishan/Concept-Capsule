f1d02b7cd418088b3f8f9c760deee936d9669755
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2021-11-30 10:05:55 +0530

    firestore connectivity completed in user profile

---
+++ lib/Widgets/CompletedCourses.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_format/date_format.dart';
+
+class CompletedCourses extends StatelessWidget {
+  final String userId;
+
+  const CompletedCourses({Key? key, required this.userId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final CollectionReference completedCourse = FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .collection('completedCourse');
+    return FutureBuilder<QuerySnapshot>(
+        future: completedCourse.get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                final DocumentSnapshot enCourse = snapshot.data!.docs[index];
+                return Card(
+                  elevation: 5,
+                  color: Colors.white,
+                  shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(15),
+                        child: Image.network(
+                          enCourse['thumbnail'],
+                          width: 90,
+                          height: 90,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            enCourse['name'],
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            enCourse['instructor'].join(', ').toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Color.fromRGBO(0, 0, 0, 0.6),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Completed on " +
+                                formatDate(enCourse['completedTime'].toDate(),
+                                    [d, ' ', M, ', ', yyyy]),
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color.fromRGBO(0, 0, 0, 0.6),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            "With " + enCourse['grade'] + " Grade",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Color.fromRGBO(0, 0, 0, 0.6),
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.connectionState == ConnectionState.done &&
+              !snapshot.hasData) {
+            return Center(
+              child: Text("No users found."),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+}
--- lib/pages/SubPages/user_profile.dart
+++ lib/pages/SubPages/user_profile.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mooc_app/Widgets/CompletedCourses.dart';
 import 'package:mooc_app/pages/login_page.dart';
 
 class UserProfile extends StatefulWidget {
@@ -14,7 +15,7 @@ class _UserProfileState extends State<UserProfile> {
   //Firebase Auth
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  late User current_user;
+  late User currentUser;
 
   Future checkAuthentication() async {
     auth.authStateChanges().listen((User? user) {
@@ -22,7 +23,7 @@ class _UserProfileState extends State<UserProfile> {
         print('User is currently signed out!');
       } else {
         print('User is signed in!');
-        current_user = user;
+        currentUser = user;
       }
     });
   }
@@ -62,157 +63,100 @@ class _UserProfileState extends State<UserProfile> {
               child: Padding(
                 padding: EdgeInsets.fromLTRB(22, 30, 22, 0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 38,
-                              backgroundImage:
-                                  NetworkImage('https://picsum.photos/76'),
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  current_user.displayName.toString(),
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 10, top: 5),
-                                  child: Text(
-                                    current_user.email.toString(),
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Color.fromRGBO(0, 0, 0, 0.63),
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 38,
+                                backgroundImage: NetworkImage(
+                                    currentUser.photoURL ??
+                                        'https://picsum.photos/76'),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      currentUser.displayName.toString(),
+                                      style: TextStyle(fontSize: 18),
                                     ),
-                                  ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 5),
+                                      child: Text(
+                                        currentUser.email.toString(),
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Color.fromRGBO(0, 0, 0, 0.63),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: logout,
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.logout_rounded,
+                                  color: Colors.black45,
+                                  size: 36.0,
+                                ),
+                                Text("Logout"),
                               ],
                             ),
-                          ],
-                        ),
-                        GestureDetector(
-                          onTap: logout,
-                          child: Column(
-                            children: [
-                              Icon(
-                                Icons.logout_rounded,
-                                color: Colors.black45,
-                                size: 36.0,
-                              ),
-                              Text("Logout"),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 18),
-                    Text(
-                      "Account Setting",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
+                          )
+                        ],
                       ),
-                    ),
-                    SizedBox(height: 12),
-                    Card(
-                      elevation: 5,
-                      color: Colors.white,
-                      shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(14),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Update Account Details"),
-                            Icon(Icons.chevron_right),
-                          ],
+                      // SizedBox(height: 18),
+                      // Text(
+                      //   "Account Setting",
+                      //   style: TextStyle(
+                      //     fontSize: 18,
+                      //     fontWeight: FontWeight.bold,
+                      //     fontStyle: FontStyle.italic,
+                      //   ),
+                      // ),
+                      // SizedBox(height: 12),
+                      // Card(
+                      //   elevation: 5,
+                      //   color: Colors.white,
+                      //   shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
+                      //   shape: RoundedRectangleBorder(
+                      //     borderRadius: BorderRadius.circular(10),
+                      //   ),
+                      //   child: Padding(
+                      //     padding: EdgeInsets.all(14),
+                      //     child: Row(
+                      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      //       children: [
+                      //         Text("Update Account Details"),
+                      //         Icon(Icons.chevron_right),
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
+                      SizedBox(height: 30),
+                      Text(
+                        "Completed Courses",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    Text(
-                      "Completed Courses",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    ListView.builder(
-                      itemCount: 4,
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return Card(
-                          elevation: 5,
-                          color: Colors.white,
-                          shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(15),
-                                child: Image.network(
-                                  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/768px-Adobe_Photoshop_CC_icon.svg.png",
-                                  width: 90,
-                                  height: 90,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Advance Photoshop",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    "Willam Scott".toUpperCase(),
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                                    ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Text(
-                                    "Completed on 15 Sep, 2021",
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                                    ),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    "With 100% Grade",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                                      fontWeight: FontWeight.bold,
-                                      fontStyle: FontStyle.italic,
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                    )
-                  ],
-                ),
+                      SizedBox(height: 16),
+                      CompletedCourses(userId: currentUser.uid)
+                      // CompletedCourses(userId: "gYPmlMV9iZffTArqlNAr")
+                    ]),
               ),
             );
           }
