fef6343a514eda0d043826d2b4a45b2e9f8883fa
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2021-12-05 23:23:16 +0530

    FEATURE: added searchbar and search function in Home Page

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-
 <h1 align="center">MOOC App Development using Flutter </h1>
 <p> Made with love by <a href="https://www.linkedin.com/in/anurag-g-a01531198/" target="_blank">Anurag Ghosh</a>, <a href="https://www.linkedin.com/in/kulkarniishan/" target="_blank">Ishan Kulkarni</a> & <a href="https://www.linkedin.com/in/dhruv-solanki-4a3a491b2/" target="_blank">Dhruv Solanki</a></p>
-
 
 <p align="center">
  <img width=200px src="assets\logo\logo-no-glow.png"  alt="Project logo" style='background-color: white'></a>
 </p>
-
 
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#-table-of-contents)
 
@@ -23,8 +20,7 @@
 ## 🧐 About <a name = "about"></a>
 
 App designed a Massively Open Online Courses (MOOC) Application that can be used by all.
-<br> 
-
+<br>
 
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#-getting-started-a-name--getting_starteda)
 
@@ -43,12 +39,10 @@ A step by step series of examples that tell you how to get a development env run
 - Clone this repository
 - Open command line or terminal in the cloned folder
 - Then select the appropriate emulator or device to run the flutter project
-  
+
   - To run the project, run `flutter run`
- 
 
 - The window in the selected device will be automatically prompted
-
 
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#-built-using-a-name--built_usinga)
 
@@ -58,18 +52,16 @@ A step by step series of examples that tell you how to get a development env run
 - [Firebase](https://firebase.google.com/) - Backend
 - [Firestore](https://firebase.google.com/docs/firestore) - Backend
 
-
-
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#-authors-a-name--authorsa)
 
 ## 📦 Packages Used <a name = "packages"></a>
 
 flutter packages
 
-
 [![-----------------------------------------------------](https://raw.githubusercontent.com/andreasbm/readme/master/assets/lines/colored.png)](#-authors-a-name--authorsa)
 
-## 	:pencil2: Authors <a name="authors"></a>
+## :pencil2: Authors <a name="authors"></a>
+
 - [Anurag Ghosh](https://www.linkedin.com/in/kulkarniishan)
 - [Ishan Kulkarni](https://www.linkedin.com/in/anurag-g-a01531198)
 - [Dhruv Solanki](https://www.linkedin.com/in/dhruv-solanki-4a3a491b2/)
--- lib/Widgets/AllCourses.dart
+++ lib/Widgets/AllCourses.dart
@@ -15,148 +15,236 @@ class _AllCoursesState extends State<AllCourses> {
   User user;
   _AllCoursesState(this.user);
 
+  final TextEditingController controller = TextEditingController();
+
   final CollectionReference<Map<String, dynamic>> AllCourses =
       FirebaseFirestore.instance.collection('course');
+
+  bool search = false;
+
+  late Query<Map<String, dynamic>> searchQuery;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Center(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Courses',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                controller: controller,
+                onChanged: (value) => {
+                  // print(value),
+                  // print(controller.text.length),
+                  setState(
+                    () {
+                      search = value.length == 0 ? false : true;
+                      searchQuery = FirebaseFirestore.instance
+                          .collection('course')
+                          .where('name', isGreaterThanOrEqualTo: value)
+                          .where('name', isLessThanOrEqualTo: value+'z');
+                    },
+                  ),
+                },
+                decoration: InputDecoration(
+                  prefixIcon: IconButton(
+                    color: Colors.black,
+                    icon: Icon(Icons.search),
+                    iconSize: 20.0,
+                    onPressed: () {
+                      //Navigator.of(context).pop();
+                    },
+                  ),
+                  contentPadding: EdgeInsets.only(left: 25.0),
+                  hintText: 'Search by Course Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          child: FutureBuilder(
-            future: AllCourses.get(),
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      final DocumentSnapshot Courses =
-                          snapshot.data!.docs[index];
-                      return InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CourseDescription(
-                                courseId: Courses.id,
-                                user: user,
-                              ),
-                            ),
-                          );
-                        },
-                        child: Card(
-                          elevation: 5,
-                          color: Colors.white,
-                          shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(15),
-                                child: Image.network(
-                                  Courses['thumbnail'],
-                                  width: 90,
-                                  height: 90,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    Courses['name'],
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    Courses['instructor'],
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                                    ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  SizedBox(height: 5),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                } else {
-                  return Center(
-                    child: Text("No Courses Found"),
-                  );
-                }
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
+          Center(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Courses',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
+          Container(
+            child: search
+                ? FutureBuilder(
+                    future: searchQuery.get(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.hasData && snapshot.data!.docs.length != 0) {
+                          return ListView.builder(
+                            itemCount: snapshot.data!.docs.length,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              final DocumentSnapshot Courses =
+                                  snapshot.data!.docs[index];
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => CourseDescription(
+                                        courseId: Courses.id,
+                                        user: user,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: Card(
+                                  elevation: 5,
+                                  color: Colors.white,
+                                  shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.all(15),
+                                        child: Image.network(
+                                          Courses['thumbnail'],
+                                          width: 90,
+                                          height: 90,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            Courses['name'],
+                                            style: TextStyle(fontSize: 20),
+                                          ),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            Courses['instructor'],
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              color:
+                                                  Color.fromRGBO(0, 0, 0, 0.6),
+                                            ),
+                                          ),
+                                          SizedBox(height: 10),
+                                          SizedBox(height: 5),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        } else {
+                          return Center(
+                            child: Text("No Courses Found"),
+                          );
+                        }
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  )
+                : FutureBuilder(
+                    future: AllCourses.get(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.hasData && snapshot.data!.docs.length != 0) {
+                          return ListView.builder(
+                            itemCount: snapshot.data!.docs.length,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              final DocumentSnapshot Courses =
+                                  snapshot.data!.docs[index];
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => CourseDescription(
+                                        courseId: Courses.id,
+                                        user: user,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: Card(
+                                  elevation: 5,
+                                  color: Colors.white,
+                                  shadowColor: Color.fromRGBO(0, 0, 0, 0.25),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.all(15),
+                                        child: Image.network(
+                                          Courses['thumbnail'],
+                                          width: 90,
+                                          height: 90,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            Courses['name'],
+                                            style: TextStyle(fontSize: 20),
+                                          ),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            Courses['instructor'],
+                                            style: TextStyle(
+                                              fontSize: 17,
+                                              color:
+                                                  Color.fromRGBO(0, 0, 0, 0.6),
+                                            ),
+                                          ),
+                                          SizedBox(height: 10),
+                                          SizedBox(height: 5),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        } else {
+                          return Center(
+                            child: Text("No Courses Found"),
+                          );
+                        }
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-  // Container(
-  //     child: ListView(
-  //       children: <Widget>[
-  //         Padding(
-  //           padding: const EdgeInsets.all(10.0),
-  //           child: TextField(
-  //             onChanged: (val) {
-  //               //initiateSearch(val);
-  //             },
-  //             decoration: InputDecoration(
-  //                 prefixIcon: IconButton(
-  //                   color: Colors.black,
-  //                   icon: Icon(Icons.search),
-  //                   iconSize: 20.0,
-  //                   onPressed: () {
-  //                     Navigator.of(context).pop();
-  //                   },
-  //                 ),
-  //                 contentPadding: EdgeInsets.only(left: 25.0),
-  //                 hintText: 'Search by Course Name',
-  //                 border: OutlineInputBorder(
-  //                     borderRadius: BorderRadius.circular(4.0))),
-  //           ),
-  //         ),
-  //         SizedBox(height: 10.0),
-  //         GridView.count(
-  //           padding: EdgeInsets.only(left: 10.0, right: 10.0),
-  //           crossAxisCount: 2,
-  //           crossAxisSpacing: 4.0,
-  //           mainAxisSpacing: 4.0,
-  //           primary: false,
-  //           shrinkWrap: true,
-  //           /*children: tempSearchStore.map((element) {
-  //               return buildResultCard(element);
-  //             }
-  //             ).toList()
-  //         )
-  //       ],
-  //     ),
-  //   ); 
